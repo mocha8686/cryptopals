4122d6347c007bcde40b45186b4726675a66257c
huecester <jericho.paulino@yahoo.com.ph>
2023-06-27 18:55:38 -0700

    Set 1 Challenge 5

--- src/data.rs
+++ src/data.rs
@@ -113,4 +113,21 @@ mod tests {
         Ok(())
     }
 
+    #[test]
+    fn repeating_key_xor() -> Result<()> {
+        let plaintext: Data =
+            "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+                .parse()?;
+        let key: Data = "ICE".parse()?;
+        let ciphertext = plaintext ^ key;
+
+        assert_eq!(
+            ciphertext,
+            Data::from_hex(
+                "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+            )?
+        );
+
+        Ok(())
+    }
 }
