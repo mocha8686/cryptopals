be2ea8e7b3afcbbc2b22613099defc339c8804bd
huecester <jericho.paulino@yahoo.com.ph>
2023-06-29 13:46:30 -0700

    Set 1 Challenge 6

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ mod data;
 use data::Data;
 use phf::phf_map;
 
+const MAX_KEYSIZE: usize = 40;
+const USE_FIRST_N_KEYSIZES: usize = 3;
+
 static LETTER_FREQUENCIES: phf::Map<char, u64> = phf_map! {
     ' ' => 999999,
     'e' => 127000,
@@ -31,6 +34,72 @@ static LETTER_FREQUENCIES: phf::Map<char, u64> = phf_map! {
     'q' => 009500,
     'z' => 007400,
 };
+
+fn break_repeating_key_xor(data: &Data) -> Data {
+    let keysizes = guess_keysizes(data);
+    keysizes[0..USE_FIRST_N_KEYSIZES]
+        .iter()
+        .map(|keysize| {
+            let keysize = *keysize;
+            let plaintext_columns: Box<_> = (0..keysize)
+                .map(|offset| get_column(data, keysize, offset))
+                .map(|ciphertext_column| guess_single_byte_xor(&ciphertext_column.into()).0)
+                .collect();
+
+            (0..plaintext_columns[0].bytes().len())
+                .map(|i| {
+                    plaintext_columns
+                        .iter()
+                        .map(move |column| column.bytes().get(i))
+                })
+                .flatten()
+                .flatten()
+                .copied()
+                .collect::<Box<[u8]>>()
+                .into()
+        })
+        .max_by_key(|data| score(data))
+        .unwrap()
+}
+
+fn get_column(data: &Data, row_length: usize, column: usize) -> Box<[u8]> {
+    data.bytes()
+        .iter()
+        .skip(column)
+        .step_by(row_length)
+        .copied()
+        .collect()
+}
+
+fn guess_keysizes(data: &Data) -> Box<[usize]> {
+    let mut keysize_guesses: Box<[(usize, usize)]> = (2..=MAX_KEYSIZE)
+        .map(|keysize| {
+            let chunks: Box<[&[u8]]> = data.bytes().chunks_exact(keysize).collect();
+            let windows = chunks.windows(2);
+
+            let hamming_distances: Box<[usize]> = windows
+                .map(|window| match window {
+                    &[lhs, rhs] => Some(hamming_distance(&lhs.into(), &rhs.into()) * 1000),
+                    _ => unreachable!(),
+                })
+                .flatten()
+                .collect();
+
+            let average_hamming_distance =
+                hamming_distances.iter().sum::<usize>() / hamming_distances.len();
+            let normalized_hamming_distance = average_hamming_distance / keysize;
+
+            (keysize, normalized_hamming_distance)
+        })
+        .collect();
+
+    keysize_guesses.sort_by_key(|(_, normalized_hamming_distance)| *normalized_hamming_distance);
+
+    keysize_guesses
+        .iter()
+        .map(|(keysize, _)| *keysize)
+        .collect()
+}
 
 fn hamming_distance(lhs: &Data, rhs: &Data) -> usize {
     assert_eq!(
@@ -115,6 +184,22 @@ mod tests {
         let rhs = "wokka wokka!!!".parse()?;
 
         assert_eq!(hamming_distance(&lhs, &rhs), 37);
+
+        Ok(())
+    }
+
+    #[test]
+    fn break_repeating_key_xor_test() -> Result<()> {
+        let input = std::fs::read_to_string("./data/1/6.txt")?
+            .trim()
+            .replace("\n", "");
+        let data = Data::from_b64(&input)?;
+        let res = break_repeating_key_xor(&data);
+
+        assert_eq!(
+            res,
+            "I'm back and I'm ringin' the bell \nA rockin' on the mike while the fly girls yell \nIn ecstasy in the back of me \nWell that's my DJ Deshay cuttin' all them Z's \nHittin' hard and the girlies goin' crazy \nVanilla's on the mike, man I'm not lazy. \n\nI'm lettin' my drug kick in \nIt controls my mouth and I begin \nTo just let it flow, let my concepts go \nMy posse's to the side yellin', Go Vanilla Go! \n\nSmooth 'cause that's the way I will be \nAnd if you don't give a damn, then \nWhy you starin' at me \nSo get off 'cause I control the stage \nThere's no dissin' allowed \nI'm in my own phase \nThe girlies sa y they love me and that is ok \nAnd I can dance better than any kid n' play \n\nStage 2 -- Yea the one ya' wanna listen to \nIt's off my head so let the beat play through \nSo I can funk it up and make it sound good \n1-2-3 Yo -- Knock on some wood \nFor good luck, I like my rhymes atrocious \nSupercalafragilisticexpialidocious \nI'm an effect and that you can bet \nI can take a fly girl and make her wet. \n\nI'm like Samson -- Samson to Delilah \nThere's no denyin', You can try to hang \nBut you'll keep tryin' to get my style \nOver and over, practice makes perfect \nBut not if you're a loafer. \n\nYou'll get nowhere, no place, no time, no girls \nSoon -- Oh my God, homebody, you probably eat \nSpaghetti with a spoon! Come on and say it! \n\nVIP. Vanilla Ice yep, yep, I'm comin' hard like a rhino \nIntoxicating so you stagger like a wino \nSo punks stop trying and girl stop cryin' \nVanilla Ice is sellin' and you people are buyin' \n'Cause why the freaks are jockin' like Crazy Glue \nMovin' and groovin' trying to sing along \nAll through the ghetto groovin' this here song \nNow you're amazed by the VIP posse. \n\nSteppin' so hard like a German Nazi \nStartled by the bases hittin' ground \nThere's no trippin' on mine, I'm just gettin' down \nSparkamatic, I'm hangin' tight like a fanatic \nYou trapped me once and I thought that \nYou might have it \nSo step down and lend me your ear \n'89 in my time! You, '90 is my year. \n\nYou're weakenin' fast, YO! and I can tell it \nYour body's gettin' hot, so, so I can smell it \nSo don't be mad and don't be sad \n'Cause the lyrics belong to ICE, You can call me Dad \nYou're pitchin' a fit, so step back and endure \nLet the witch doctor, Ice, do the dance to cure \nSo come up close and don't be square \nYou wanna battle me -- Anytime, anywhere \n\nYou thought that I was weak, Boy, you're dead wrong \nSo come on, everybody and sing this song \n\nSay -- Play that funky music Say, go white boy, go white boy go \nplay that funky music Go white boy, go white boy, go \nLay down and boogie and play that funky music till you die. \n\nPlay that funky music Come on, Come on, let me hear \nPlay that funky music white boy you say it, say it \nPlay that funky music A little louder now \nPlay that funky music, white boy Come on, Come on, Come on \nPlay that funky music \n".parse()?
+        );
 
         Ok(())
     }
