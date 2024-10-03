600d405a0a1921b7bcce2bc4430cf69eb250770e
mocha8686 <june.mocha8686@gmail.com>
2024-10-03 11:21:29 -0700

    1-2

--- src/data.zig
+++ src/data.zig
@@ -7,6 +7,15 @@ const Data = struct {
 
     const Self = @This();
 
+    pub fn init(allocator: Allocator, data: []const u8) Self {
+        const buf = try allocator.alloc(u8, data.len);
+        std.mem.copyForwards(u8, buf, data);
+        return Self{
+            .data = buf,
+            .allocator = allocator,
+        };
+    }
+
     pub fn fromHex(allocator: Allocator, hex_str: []const u8) !Self {
         if (hex_str.len % 2 != 0) {
             return error.BadLength;
@@ -30,6 +39,22 @@ const Data = struct {
         return Self{
             .data = data,
             .allocator = allocator,
+        };
+    }
+
+    pub fn xor(self: *const Self, other: *const Self) !Self {
+        if (self.data.len != other.data.len) {
+            return error.LengthMismatch;
+        }
+
+        const buf = try self.allocator.alloc(u8, self.data.len);
+        for (self.data, other.data, 0..) |l, r, i| {
+            buf[i] = l ^ r;
+        }
+
+        return Self{
+            .data = buf,
+            .allocator = self.allocator,
         };
     }
 
@@ -80,14 +105,36 @@ test "challenge 1" {
     const base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
     const hex_to_base64 = try Data.fromHex(allocator, hex);
-    defer hex_to_base64.deinit();
     const test_base64 = try hex_to_base64.base64();
+
+    defer hex_to_base64.deinit();
     defer test_base64.deinit();
+
     try std.testing.expectEqualStrings(base64, test_base64.data);
 
     const base64_to_hex = try Data.fromBase64(allocator, base64);
-    defer base64_to_hex.deinit();
     const test_hex = try base64_to_hex.hex();
+
+    defer base64_to_hex.deinit();
     defer test_hex.deinit();
+
     try std.testing.expectEqualStrings(hex, test_hex.data);
+}
+
+test "challenge 2" {
+    const allocator = std.testing.allocator;
+
+    const lhs = try Data.fromHex(allocator, "1c0111001f010100061a024b53535009181c");
+    const rhs = try Data.fromHex(allocator, "686974207468652062756c6c277320657965");
+
+    defer lhs.deinit();
+    defer rhs.deinit();
+
+    const res = try lhs.xor(&rhs);
+    const res_str = try res.hex();
+
+    defer res.deinit();
+    defer res_str.deinit();
+
+    try std.testing.expectEqualStrings("746865206b696420646f6e277420706c6179", res_str.data);
 }
