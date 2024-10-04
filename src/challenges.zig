277d7a1aad5965dea1b1ccc73a1576cf8863e376
mocha8686 <june.mocha8686@gmail.com>
2024-10-04 10:56:44 -0700

    1-4

--- src/challenges.zig
+++ src/challenges.zig
@@ -50,3 +50,35 @@ test "challenge 3" {
 
     try std.testing.expectEqualStrings("Cooking MC's like a pound of bacon", res.data);
 }
+
+test "challenge 4" {
+    const challenge_text = @embedFile("data/1/4.txt");
+    const trimmed = std.mem.trim(u8, challenge_text, " \n\r\t");
+    var iter = std.mem.splitSequence(u8, trimmed, "\n");
+
+    var max_score: isize = 0;
+    var best_guess: ?Data = null;
+    while (iter.next()) |hex_str| {
+        const data = try Data.fromHex(allocator, hex_str);
+        defer data.deinit();
+
+        const guess = try data.guess_repeating_key_xor();
+        const score = guess.score();
+
+        if (score > max_score) {
+            if (best_guess) |old_guess| {
+                old_guess.deinit();
+            }
+
+            max_score = score;
+            best_guess = guess;
+        } else {
+            guess.deinit();
+        }
+    }
+
+    const final_guess = best_guess orelse unreachable;
+    defer final_guess.deinit();
+
+    try std.testing.expectEqualStrings("Now that the party is jumping\n", final_guess.data);
+}
