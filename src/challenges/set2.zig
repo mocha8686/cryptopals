d4d6b91f9fb701235a90ae2efdee59e9aa21de32
mocha8686 <june.mocha8686@gmail.com>
2024-11-13 23:02:43 -0800

    2-12

--- src/challenges/set2.zig
+++ src/challenges/set2.zig
@@ -3,10 +3,13 @@ const config = @import("config");
 const cryptopals = @import("cryptopals");
 const blackbox = @import("set2/blackbox.zig");
 
+const allocator = std.testing.allocator;
+
 const aesEcbOrCbc = blackbox.aesEcbOrCbc;
+const AesPrefix = blackbox.AesPrefix;
+
 const Data = cryptopals.Data;
 const oracle = cryptopals.oracle;
-const allocator = std.testing.allocator;
 
 test "challenge 9" {
     var data = try Data.new(allocator, "YELLOW SUBMARINE");
@@ -67,3 +70,83 @@ test "[slow] challenge 11 x100" {
     }
 }
 
+test "challenge 12" {
+    const prefix_blackbox = try AesPrefix.new();
+
+    const block_size = blk: {
+        inline for (1..64) |i| {
+            const payload = "AA" ** i;
+            var data = try Data.new(allocator, payload);
+            defer data.deinit();
+            try prefix_blackbox.encrypt(&data);
+            if (std.mem.eql(u8, data.buf[0..i], data.buf[i .. i * 2])) break :blk i;
+        }
+        std.debug.panic("Could not find block size between 0 and 64.", .{});
+    };
+
+    try std.testing.expectEqual(16, block_size);
+
+    const info = blk: {
+        var empty = try Data.new(allocator, "");
+        defer empty.deinit();
+        try prefix_blackbox.encrypt(&empty);
+        const next_block_len = empty.buf.len;
+
+        for (1..block_size + 1) |i| {
+            var payload = try allocator.alloc(u8, i);
+            defer allocator.free(payload);
+            @memset(payload[0..], 'A');
+
+            var data = try Data.new(allocator, payload);
+            defer data.deinit();
+            try prefix_blackbox.encrypt(&data);
+            if (data.buf.len != next_block_len) {
+                break :blk .{
+                    .next_block_len = next_block_len,
+                    .ciphertext_len = next_block_len - i,
+                };
+            }
+        }
+        unreachable;
+    };
+    const next_block_len = info.next_block_len;
+    const ciphertext_len = info.ciphertext_len;
+
+    var res = try allocator.alloc(u8, ciphertext_len);
+    defer allocator.free(res);
+
+    var buf = try allocator.alloc(u8, next_block_len);
+    defer allocator.free(buf);
+    @memset(buf, 'A');
+
+    const b = ciphertext_len;
+    const a = b - block_size;
+
+    outer: for (0..ciphertext_len) |i| {
+        var data = try Data.new(allocator, buf[0 .. buf.len - i - 1]);
+        defer data.deinit();
+        try prefix_blackbox.encrypt(&data);
+        const target = data.buf[a..b];
+
+        std.mem.copyForwards(u8, buf[buf.len - i - 1 .. buf.len - 1], buf[buf.len - i ..]);
+        for (0..std.math.maxInt(u8)) |n| {
+            const c: u8 = @intCast(n);
+
+            buf[buf.len - 1] = c;
+            var guess = try Data.new(allocator, buf);
+            defer guess.deinit();
+            try prefix_blackbox.encrypt(&guess);
+
+            if (std.mem.eql(u8, guess.buf[a..b], target)) {
+                res[i] = c;
+                continue :outer;
+            }
+        }
+        unreachable;
+    }
+
+    try std.testing.expectEqualStrings(
+        @embedFile("data/2/12-sol.txt"),
+        res,
+    );
+}
--- src/challenges/set2/blackbox.zig
+++ src/challenges/set2/blackbox.zig
@@ -45,3 +45,44 @@ pub fn aesEcbOrCbc(allocator: Allocator, maybe_cipher_type: ?EcbOrCbc) !Data {
     try data.encrypt(cipher_type);
     return data;
 }
+
+pub const AesPrefix = struct {
+    key: [16]u8,
+
+    const hidden_string = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK";
+
+    const Self = @This();
+
+    pub fn new() !Self {
+        var seed: u64 = undefined;
+        try std.posix.getrandom(std.mem.asBytes(&seed));
+        var prng = std.rand.DefaultPrng.init(seed);
+        const rand = prng.random();
+
+        var key: [16]u8 = undefined;
+        rand.bytes(&key);
+
+        return Self{
+            .key = key,
+        };
+    }
+
+    pub fn encrypt(self: Self, data: *Data) !void {
+        const allocator = data.allocator;
+
+        const hidden_plaintext = try Data.fromBase64(allocator, hidden_string);
+        defer hidden_plaintext.deinit();
+
+        const len = data.buf.len + hidden_plaintext.buf.len;
+        const plaintext = try allocator.alloc(u8, len);
+
+        @memcpy(plaintext[0..data.buf.len], data.buf);
+        @memcpy(plaintext[data.buf.len..], hidden_plaintext.buf);
+
+        data.deinit();
+        data.buf = plaintext;
+
+        try data.pad(16);
+        try data.encrypt(.{ .aes_128_ecb = .{ .key = self.key } });
+    }
+};
