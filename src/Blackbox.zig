ee822813fe7e694f28e4dddc66d1c5c70f044624
mocha8686 <june.mocha8686@gmail.com>
2025-06-16 16:41:42 -0700

    todo: 3-17

--- src/Blackbox.zig
+++ src/Blackbox.zig
@@ -1,3 +1,4 @@
 pub const Encrypter = @import("blackbox/Encrypter.zig");
 pub const Decrypter = @import("blackbox/Decrypter.zig");
 pub const EncDec = @import("blackbox/EncDec.zig");
+pub const PaddingOracle = @import("blackbox/PaddingOracle.zig");
---
+++ src/blackbox/PaddingOracle.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+const Data = @import("../Data.zig");
+const cipherLib = @import("../cipher.zig");
+
+const Allocator = std.mem.Allocator;
+
+ptr: *anyopaque,
+hasValidPaddingPtr: *const fn (ptr: *anyopaque, data: Data) anyerror!bool,
+
+const Self = @This();
+
+pub fn init(ptr: anytype) Self {
+    const T = @TypeOf(ptr);
+    const type_info = @typeInfo(T);
+
+    if (type_info != .Pointer) @compileError("ptr must be a pointer");
+    if (type_info.Pointer.size != .One) @compileError("ptr must be a single item pointer");
+
+    const gen = struct {
+        pub fn hasValidPadding(self_ptr: *anyopaque, data: Data) anyerror!bool {
+            const self: T = @ptrCast(@alignCast(self_ptr));
+            return @call(.always_inline, type_info.Pointer.child.hasValidPadding, .{ self, data });
+        }
+    };
+
+    return .{
+        .ptr = ptr,
+        .hasValidPaddingPtr = gen.hasValidPadding,
+    };
+}
+
+pub fn hasValidPadding(self: Self, data: Data) !bool {
+    return self.hasValidPaddingPtr(self.ptr, data);
+}
