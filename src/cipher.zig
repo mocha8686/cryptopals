92eeb9bedafdcd7dd952bec0865ad632da63f23e
mocha8686 <june.mocha8686@gmail.com>
2025-11-17 22:19:14 -0800

    feat: AES128-ECB

--- src/cipher.zig
+++ src/cipher.zig
@@ -1,3 +1,4 @@
+pub const AES = @import("cipher/AES.zig");
 pub const Base64 = @import("cipher/Base64.zig");
 pub const Hex = @import("cipher/Hex.zig");
 pub const XOR = @import("cipher/XOR.zig");
---
+++ src/cipher/AES.zig
@@ -0,0 +1,63 @@
+const std = @import("std");
+const Data = @import("../Data.zig");
+
+const Self = @This();
+
+fn Key(blocksize: comptime_int) type {
+    return [blocksize / 16]u8;
+}
+
+pub const Mode = union(enum) {
+    ECB: struct { key: Key(128) },
+};
+
+mode: Mode,
+
+pub fn decode(self: Self, data: *Data) !void {
+    switch (self.mode) {
+        .ECB => |c| encodeECB(data, c.key),
+    }
+}
+
+pub fn encode(self: Self, data: *Data) !void {
+    switch (self.mode) {
+        .ECB => |c| decodeECB(data, c.key),
+    }
+}
+
+fn decodeECB(data: *Data, key: Key(128)) !void {
+    const allocator = data.allocator;
+    const blocksize = 16;
+
+    const aes = std.crypto.core.aes.Aes128.initDec(key);
+    var res = try allocator.alloc(u8, data.len());
+
+    var windows = std.mem.window(u8, data.bytes, blocksize, blocksize);
+    var i: u32 = 0;
+    while (windows.next()) |block| {
+        aes.decrypt(&res[i * blocksize], block);
+        i += 1;
+    }
+
+    data.reinit(res);
+    try data.unpad(blocksize);
+}
+
+fn encodeECB(data: *Data, key: Key(128)) !void {
+    const allocator = data.allocator;
+    const blocksize = 16;
+
+    try data.pad(blocksize);
+
+    const aes = std.crypto.core.aes.Aes128.initEnc(key);
+    var res = try allocator.alloc(u8, data.len());
+
+    var windows = std.mem.window(u8, data.bytes, blocksize, blocksize);
+    var i: u32 = 0;
+    while (windows.next()) |block| {
+        aes.encrypt(&res[i * blocksize], block);
+        i += 1;
+    }
+
+    data.reinit(res);
+}
