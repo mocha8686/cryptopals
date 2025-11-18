a1d70c96d048d2d6f07ea8e27d217d081f58045d
mocha8686 <june.mocha8686@gmail.com>
2025-11-17 16:41:27 -0800

    feat: 1-2

--- src/root.zig
+++ src/root.zig
@@ -22,3 +22,29 @@ test "set 1 challenge 1" {
         data.bytes,
     );
 }
+
+test "set 1 challenge 2" {
+    const allocator = std.testing.allocator;
+
+    var lhs = try Data.fromHex(
+        allocator,
+        "1c0111001f010100061a024b53535009181c",
+    );
+    defer lhs.deinit();
+
+    const rhs = try Data.fromHex(
+        allocator,
+        "686974207468652062756c6c277320657965",
+    );
+    defer rhs.deinit();
+
+    try lhs.xor(rhs);
+
+    const hex = Hex{};
+    try lhs.encode(hex);
+
+    try std.testing.expectEqualStrings(
+        "746865206b696420646f6e277420706c6179",
+        lhs.bytes,
+    );
+}
