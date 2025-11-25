30ee2ebb562e03cc9ccb7f7b68342f30a762d2c8
mocha8686 <june.mocha8686@gmail.com>
2025-11-24 21:34:31 -0800

    feat: 1-1

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,21 @@ pub mod error;
 
 pub use data::Data;
 pub use error::{Error, Result};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn s1c1_convert_hex_to_base64() -> Result<()> {
+        let res = Data::from_hex_str("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")?
+            .base64();
+
+        assert_eq!(
+            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
+            res
+        );
+
+        Ok(())
+    }
+}
