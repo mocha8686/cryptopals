5e4fd90b25b409e2d48cfd1c3cac8df053541d19
mocha8686 <june.mocha8686@gmail.com>
2025-11-24 22:18:28 -0800

    feat: 1-5

--- src/data/xor.rs
+++ src/data/xor.rs
@@ -82,4 +82,18 @@ mod tests {
 
         Ok(())
     }
+
+    #[test]
+    fn s1c5_implement_repeating_key_xor() {
+        let data = Data::from(
+            "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal".as_bytes(),
+        );
+        let key = Data::from("ICE".as_bytes());
+
+        let res = data ^ key;
+        assert_eq!(
+            "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f",
+            res.hex()
+        );
+    }
 }
